06f387f72c04fcdf5ccb1f44f199e7edb73f5a72
Leonardo Teixeira Alves <alves.leonardo.t@gmail.com>
2022-03-04 11:10:06 -0300

    Image downscaling done

--- project1.py
+++ project1.py
@@ -1,20 +1,95 @@
+from math import floor
+from collections import Counter
 import cv2 as cv
 import numpy as np
 
+# function to spread m elements evenly in a sequence from 0 to n-1 (a.k.a. range(n))
+# (source: https://stackoverflow.com/questions/9873626/choose-m-evenly-spaced-elements-from-a-sequence-of-length-n)
+spread = lambda m, n: [i*n//m + n//(2*m) for i in range(m)]
 
 def imageScaling():
 
     # Load the image
-    img = cv.imread("images/notre-dame.pgm", cv.IMREAD_GRAYSCALE)
+    resizedImg = img = cv.imread("images/notre-dame.pgm", cv.IMREAD_GRAYSCALE)
+
+    # Initialize the scale factor
+    scaleFactor = 0
+
+    # Read scale factor until it is valid (i.e. greater than 0)
+    while scaleFactor <= 0:
+        try:
+            scaleFactor = float(input("Enter a scale factor: "))
+        except ValueError:
+            print("Invalid input. Please enter a number greater than 0.")
+
+        if scaleFactor <= 0:
+            print("Invalid input. Please enter a number greater than 0.")
+
+    # Get original image dimensions
+    originalWidth, originalHeight = img.shape[:2]
+
+    if scaleFactor < 1:
+
+        # Calculate how many pixels to remove from each side of the image
+        widthToDelete, heigthToDelete = floor((1 - scaleFactor) * originalWidth), floor((1 - scaleFactor) * originalHeight)
+
+        # Create list of indices to delete
+        rowsToDelete = spread(widthToDelete, originalWidth)
+        colsToDelete = spread(heigthToDelete, originalHeight)
+
+        # Delete the pixels
+        resizedImg = np.delete(resizedImg, rowsToDelete, axis=0)
+        resizedImg = np.delete(resizedImg, colsToDelete, axis=1)
+    elif scaleFactor > 1: 
+
+        # Calculate new size of the image
+        newWidth, newHeight = floor(originalWidth * scaleFactor), floor(originalHeight * scaleFactor)
+
+        # Get dict of indices to replicate and how many times to replicate
+        rowsToClone = Counter(spread(newWidth, originalWidth))
+        colsToClone = Counter(spread(newHeight, originalHeight))
+
+        # TODO: Implement nearest neighbor interpolation
+
+
+    # resizedImg = cv.resize(img, (0, 0), fx=scaleFactor, fy=scaleFactor)
+
+    originalImageWindowName = "Original image"
+    resizedImageWindowName = "Resized image"
 
     # Create a window to display the image
-    cv.namedWindow("Original image", cv.WINDOW_AUTOSIZE)
+    cv.namedWindow(originalImageWindowName, cv.WINDOW_NORMAL)
+    #Force focus on new window
+    cv.setWindowProperty(originalImageWindowName, cv.WND_PROP_TOPMOST, 1)
     # Display the image
-    cv.imshow("Original image", img)
+    cv.imshow(originalImageWindowName, img)
+
+    # Create a window to display the resized image
+    cv.namedWindow(resizedImageWindowName, cv.WINDOW_NORMAL)
+    #Force focus on new window
+    cv.setWindowProperty(resizedImageWindowName, cv.WND_PROP_TOPMOST, 1)
+    # Display the resized image
+    cv.imshow(resizedImageWindowName, resizedImg)
+
+    cv.resizeWindow(originalImageWindowName, img.shape[1], img.shape[0])
+    cv.resizeWindow(resizedImageWindowName, resizedImg.shape[1], resizedImg.shape[0])
+
+    print(f"Original image size: {img.shape[0]} x {img.shape[1]}")
+    print(f"Resized image size: {resizedImg.shape[0]} x {resizedImg.shape[1]}")
+
     # Wait for a key to be pressed
+    print("\nPress any key to go back to main menu.\n")
     cv.waitKey(0)
-    # Destroy the window
-    cv.destroyWindow("Original image")
+
+    # Destroy all windows
+    try:
+        cv.destroyWindow(originalImageWindowName)
+    except cv.error:
+        pass
+    try:
+        cv.destroyWindow(resizedImageWindowName)
+    except cv.error:
+        pass
 
 def homomorphicFiltering():
     print("Not yet implemented")
