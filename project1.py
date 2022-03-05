49e6e508c72a2a0872400ea98bd04be7b5807b5f
Leonardo Teixeira Alves <alves.leonardo.t@gmail.com>
2022-03-05 18:51:43 -0300

    Completed homomorphic filter (part 2)

--- project1.py
+++ project1.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from datetime import datetime
 
 # function to spread m elements evenly in a sequence from 0 to n-1 (a.k.a. range(n))
 # (source: https://stackoverflow.com/questions/9873626/choose-m-evenly-spaced-elements-from-a-sequence-of-length-n)
@@ -56,14 +57,14 @@ def imageScaling():
 
     # Create a window to display the image
     cv.namedWindow(originalImageWindowName, cv.WINDOW_NORMAL)
-    #Force focus on new window
+    # Force focus on new window
     cv.setWindowProperty(originalImageWindowName, cv.WND_PROP_TOPMOST, 1)
     # Display the image
     cv.imshow(originalImageWindowName, img)
 
     # Create a window to display the resized image
     cv.namedWindow(resizedImageWindowName, cv.WINDOW_NORMAL)
-    #Force focus on new window
+    # Force focus on new window
     cv.setWindowProperty(resizedImageWindowName, cv.WND_PROP_TOPMOST, 1)
     # Display the resized image
     cv.imshow(resizedImageWindowName, resizedImg)
@@ -74,26 +75,163 @@ def imageScaling():
     print(f"Original image size: {img.shape[0]} x {img.shape[1]}")
     print(f"Resized image size: {resizedImg.shape[0]} x {resizedImg.shape[1]}")
 
+    # Save the resized image
+    saveLocation = f"images/results/notre-dame-resized-{datetime.now().strftime('%Y-%m-%d-%H_%M_%S')}.pgm"
+    cv.imwrite(saveLocation, resizedImg)
+    print(f"Image saved to {saveLocation}")
+
     # Wait for a key to be pressed
     print("\nPress any key to go back to main menu.\n")
     cv.waitKey(0)
 
     # Destroy all windows
     try:
-        cv.destroyWindow(originalImageWindowName)
-    except cv.error:
-        pass
-    try:
-        cv.destroyWindow(resizedImageWindowName)
+        cv.destroyAllWindows()
     except cv.error:
         pass
 
 def homomorphicFiltering():
-    print("Not yet implemented")
-    #https://cadubentzen.github.io/pdi-ufrn/unit2/homomorphic.html
+
+    # Load the image
+    img = cv.imread("images/image2.jpg", cv.IMREAD_GRAYSCALE)
+    
+    # Take image dimensions
+    height, width = img.shape[:2]
+
+    # Parameters for homomorphic filter
+    gamma_l = gamma_h = sigma = -1
+
+    # Read gamma_l until it is valid (i.e. from 0 to 10)
+    while gamma_l < 0 or gamma_l > 10:
+        try:
+            gamma_l = float(input("Enter value for gamma_l: "))
+        except ValueError:
+            print("Invalid input. Please enter a number from 0 to 10")
+
+        if gamma_l < 0 or gamma_l > 10:
+            print("Invalid input. Please enter a number from 0 to 10")
+
+    # Read gamma_h until it is valid (i.e. from 0 to 10)
+    while gamma_h < 0 or gamma_h > 10:
+        try:
+            gamma_h = float(input("Enter value for gamma_h: "))
+        except ValueError:
+            print("Invalid input. Please enter a number from 0 to 10")
+
+        if gamma_h < 0 or gamma_h > 10:
+            print("Invalid input. Please enter a number from 0 to 10")
+
+    # Read sigma until it is valid (i.e. from 1 to 100)
+    while sigma < 1 or sigma > 100:
+        try:
+            sigma = int(input("Enter value for sigma: "))
+        except ValueError:
+            print("Invalid input. Please enter a number from 1 to 100")
+
+        if sigma < 1 or sigma > 100:
+            print("Invalid input. Please enter a number from 1 to 100")
+
+    # Take the natural log of image, do DFT and shift it
+    img_log = np.log(np.float32(img) + 0.01)
+    img_dft = np.fft.fft2(img_log)
+    img_dft_shift = np.fft.fftshift(img_dft)
+
+    # Get coordinates of each pixel
+    x, y = np.meshgrid(np.arange(width), np.arange(height))
+
+    # Coordinates of the center of the image
+    xCenter, yCenter = width // 2, height // 2
+
+    distSquared = np.square(x - xCenter) + np.square(y - yCenter)
+
+    c = 0.1
+
+    G = (gamma_h - gamma_l) * (1 - np.exp(c * (- distSquared / (2 * sigma**2)))) + gamma_l
+
+    # Invert the initial steps to get image back
+    result_filtered = G * img_dft_shift
+    result_mid = np.real(np.fft.ifft2(np.fft.ifftshift(result_filtered)))
+    filteredImage = np.exp(result_mid)
+    filteredImage = cv.normalize(filteredImage, None, 0, 255, cv.NORM_MINMAX).astype(np.uint8)
+
+    originalImageWindowName = "Original image"
+    filteredImageWindowName = "Filtered image"
+
+    # Create a window to display the image
+    cv.namedWindow(originalImageWindowName, cv.WINDOW_NORMAL)
+    # Force focus on new window
+    cv.setWindowProperty(originalImageWindowName, cv.WND_PROP_TOPMOST, 1)
+    # Display the image
+    cv.imshow(originalImageWindowName, img)
+
+    # Create a window to display the homomomorphic filtered image
+    cv.namedWindow(filteredImageWindowName, cv.WINDOW_NORMAL)
+    # Force focus on new window
+    cv.setWindowProperty(filteredImageWindowName, cv.WND_PROP_TOPMOST, 1)
+    # Display the homomomorphic filtered image
+    cv.imshow(filteredImageWindowName, filteredImage)
+
+    cv.resizeWindow(originalImageWindowName, img.shape[1], img.shape[0])
+    cv.resizeWindow(filteredImageWindowName, filteredImage.shape[1], filteredImage.shape[0])
+
+    # Save the homomomorphic filtered image
+    saveLocation = f"images/results/image2-homomorphic-{datetime.now().strftime('%Y-%m-%d-%H_%M_%S')}.jpg"
+    cv.imwrite(saveLocation, filteredImage)
+    print(f"Image saved to {saveLocation}")
+
+    # Wait for a key to be pressed
+    print("\nPress any key to go back to main menu.\n")
+    cv.waitKey(0)
+
+    # Destroy all windows
+    try:
+        cv.destroyAllWindows()
+    except cv.error:
+        pass
 
 def notchRejectFilter():
-    print("Not yet implemented")
+
+    # Load the image
+    img = cv.imread("images/moire.tif", cv.IMREAD_GRAYSCALE)
+    
+    # Take image dimensions
+    height, width = img.shape[:2]
+
+    originalImageWindowName = "Original image"
+    # filteredImageWindowName = "Filtered image"
+
+    # Create a window to display the image
+    cv.namedWindow(originalImageWindowName, cv.WINDOW_NORMAL)
+    #Force focus on new window
+    cv.setWindowProperty(originalImageWindowName, cv.WND_PROP_TOPMOST, 1)
+    # Display the image
+    cv.imshow(originalImageWindowName, img)
+
+    # # Create a window to display the notch rejected filtered image
+    # cv.namedWindow(filteredImageWindowName, cv.WINDOW_NORMAL)
+    # #Force focus on new window
+    # cv.setWindowProperty(filteredImageWindowName, cv.WND_PROP_TOPMOST, 1)
+    # # Display the notch rejected filtered image
+    # cv.imshow(filteredImageWindowName, filteredImage)
+
+    cv.resizeWindow(originalImageWindowName, img.shape[1], img.shape[0])
+    # cv.resizeWindow(filteredImageWindowName, filteredImage.shape[1], filteredImage.shape[0])
+
+    # # Save the homomomorphic filtered image
+    # saveLocation = f"images/results/image2-homomorphic-{datetime.now().strftime('%Y-%m-%d-%H_%M_%S')}.jpg"
+    # cv.imwrite(saveLocation, filteredImage)
+    # print(f"Image saved to {saveLocation}")
+
+    # Wait for a key to be pressed
+    print("\nPress any key to go back to main menu.\n")
+    cv.waitKey(0)
+
+    # Destroy all windows
+    try:
+        cv.destroyAllWindows()
+    except cv.error:
+        pass
+
 
 if __name__ == "__main__":
 
